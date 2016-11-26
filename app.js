85e980dd955f789016a004365c1db5f9c552967d
dalaka <dalaka@aims.ac.tz>
2016-11-26 07:06:07 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
 
+(function (){
+angular.module('LunchChecker',[])
+
+.controller('LunchCheckController', function($scope){
+$scope.list_lunch =''
+$scope.fail='Please enter data first'
+$scope.enjoy='Enjoy!'
+$scope.much='Too much!'
+function checkTextField() {
+     var words = $scope.list_lunch.split(",");
+    if (words == '') {
+        $scope.fail;
+        
+    }
+    if (words <=3) {
+        $scope.enjoy;
+        
+    }
+    if (words >3) {
+        $scope.much;
+        
+    }
+}
+
+})
+
+})();
