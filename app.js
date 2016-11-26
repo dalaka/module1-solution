6b18f3053ad435718f55f244901b6262110eecc7
dalaka <dalaka@aims.ac.tz>
2016-11-26 07:24:15 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,10 +3,10 @@
 angular.module('LunchChecker',[])
 
 .controller('LunchCheckController', function($scope){
-$scope.list_lunch =''
-$scope.fail='Please enter data first'
-$scope.enjoy='Enjoy!'
-$scope.much='Too much!'
+$scope.list_lunch ='';
+$scope.fail='Please enter data first';
+$scope.enjoy='Enjoy!';
+$scope.much='Too much!';
 function checkLunch() {
      var words = $scope.list_lunch.split(",");
     if (words == '') {
